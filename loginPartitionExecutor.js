d6efd94942999664867f80fc9ec2dcc39d0990c9
d.mustaev <d.mustaev@gmail.com>
2018-05-10 00:10:30 +0500

    1

--- loginPartitionExecutor.js
+++ loginPartitionExecutor.js
@@ -15,13 +15,51 @@ module.exports = function (I, vars) {
             }
 
             if (config.isAsync === true && !!config.loginScript) {
-                cookies = await require(cookiePath);
-                await console.log('Login with stored cookies ' + cookies.length);
+                let cookies = await require(cookiePath);
+                console.log('Login with stored cookies ' + cookies.length);
+                cookies = JSON.stringify(cookies);
                 await I.amOnPage('/');
                 await I.clearCookie();
-                for (let cookie of cookies) {
-                    await I.setCookie(cookie);
-                }
+                await I.executeScript(function (cookies) {
+                    cookies = JSON.parse(cookies);
+                    cookies.forEach(function (cookie) {
+                        setCookie(cookie.name, cookie.value, {
+                            domain: cookie.domain,
+                            path: cookie.path,
+                            secure: cookie.secure
+                        })
+                    });
+                    function setCookie(name, value, options) {
+                        options = options || {};
+
+                        let expires = options.expires;
+
+                        if (typeof expires == "number" && expires) {
+                            let d = new Date();
+                            d.setTime(d.getTime() + expires * 1000);
+                            expires = options.expires = d;
+                        }
+                        if (expires && expires.toUTCString) {
+                            options.expires = expires.toUTCString();
+                        }
+
+                        value = encodeURIComponent(value);
+
+                        let updatedCookie = name + "=" + value;
+
+                        for (let propName in options) {
+                            updatedCookie += "; " + propName;
+                            let propValue = options[propName];
+                            if (propValue !== true) {
+                                updatedCookie += "=" + propValue;
+                            }
+                        }
+                        document.cookie = updatedCookie;
+                    }
+                }, cookies);
+                // for (let cookie of cookies) {
+                //     await I.setCookie(cookie);
+                // }
             }
         },
     });
