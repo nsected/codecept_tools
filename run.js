294528e11351e93ec08d5c96b9fb9d82d67f92a3
d.mustaev <d.mustaev@gmail.com>
2018-07-10 00:55:00 +0500

    v4

--- run.js
+++ run.js
@@ -53,7 +53,7 @@ async function run(cmd) {
     let overrideConfig = JSON.parse(cmd.override);
     let config = require(path.join(process.cwd(), configPath)).config;
     config = merge(config, overrideConfig);
-    config.codeceptParams = cmd.params;
+    config.codeceptParams = cmd.params | [];
     config.isAsync = cmd.async;
     config.isVerbose = cmd.verbose;
     if (!Number.isInteger(config.threadsLimit)) config.threadsLimit = 1;
