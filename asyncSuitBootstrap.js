800d121ce66d23cede50f769f2639fc2c20d2313
d.mustaev <d.mustaev@xsolla.com>
2018-07-09 15:56:11 +0500

    Custom Runner

--- asyncSuitBootstrap.js
+++ asyncSuitBootstrap.js
@@ -8,8 +8,10 @@ const cookiePath = path.join(tmp, '/cookies.json');
 
 Feature('Preparation', {timeout: config.timeout, retries: config.retries});
 
-Scenario('Preparation', async (I, vars) => {
+Scenario('Preparation', async (I) => {
         try {
+            rimraf.sync(tmp, {}, function () {});
+
             if (!!config.suiteBootstrap) {
                 const bootstrapPartition = path.join(process.cwd(), path.dirname( config.mocha.config), config.suiteBootstrap);
                 const bootstrap = await require(bootstrapPartition);
@@ -19,12 +21,6 @@ Scenario('Preparation', async (I, vars) => {
             if (!!config.login && !!config.isAsync) {
                 let loginPartition = path.join(process.cwd(), path.dirname( config.mocha.config), config.login);
                 let login = await require(loginPartition);
-
-                if (!fs.existsSync(dir)){
-                    fs.mkdirSync(dir);
-                } else if (path.existsSync(cookiePath)) {
-                    rimraf.sync(cookiePath, {}, function () {});
-                }
 
                 console.log('Login with scenario: ' + config.login);
                 await login(I, config);
